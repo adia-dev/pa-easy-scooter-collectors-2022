56f5157128d8a994c28458da5aa4457bc909cbaf
Abdoulaye Dia <abdoulaye@jgandco.consulting>
2022-07-13 19:59:23 +0200

    begin Homescreen

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Provider } from "react-redux";
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Lets build UBER</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
