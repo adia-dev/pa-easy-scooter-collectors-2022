d22e2517ca1a91d6c51cce9def91051116560301
Abdoulaye Dia <abdoulaye@jgandco.consulting>
2022-07-13 19:17:11 +0200

    Setup redux + store

---
+++ store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+
+// Reducers and Slices
+import navReducer from './slices/navSlice'
+
+
+export const store = configureStore({ reducer: navReducer })
