63a2d7e727ded83a3905015adf38579348e49889
Abdoulaye Dia <abdoulaye@jgandco.consulting>
2022-07-13 20:10:20 +0200

    Homescreen update

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
+import { Icon } from 'react-native-elements'
 import tw from 'tailwind-react-native-classnames'
 
 
@@ -30,8 +31,13 @@ const NavOptions = () => {
                 <TouchableOpacity
                     style={tw`pr-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                     <View>
-                        <Image style={{ width: 120, aspectRatio: '1/1', resizeMode: 'contain' }} source={{ uri: item.image }} />
+                        <Image style={{ width: 120, aspectRatio: 1 / 1, resizeMode: 'contain' }} source={{ uri: item.image }} />
                         <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                        <Icon
+                            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                            type='antdesign'
+                            name='arrowright'
+                            color='white' />
                     </View>
                 </TouchableOpacity>
             )}
